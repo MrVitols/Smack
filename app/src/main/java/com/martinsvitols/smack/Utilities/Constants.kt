0a1d2d2fee5744d218e074bd905de9f4ae82d047
Vitols, Martins <martins.vitols@accenture.com>
2018-08-28 10:14:20 +0300

    lesson 12

--- app/src/main/java/com/martinsvitols/smack/Utilities/Constants.kt
+++ app/src/main/java/com/martinsvitols/smack/Utilities/Constants.kt
@@ -3,4 +3,5 @@ package com.martinsvitols.smack.Utilities
 const val BASE_URL = "https://chatsman.herokuapp.com/v1/"
 const val URL_REGISTER = "${BASE_URL}account/register"
 const val URL_LOGIN = "${BASE_URL}account/login"
+const val URL_CREATE_USER = "${BASE_URL}user/add"
 
